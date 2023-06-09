3651fa04234ed88713ff497d6d08b299f0ade05a
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-09 13:56:09 +0530

    Add project files.

---
+++ ASSIGN5/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//5.Write a program to calculate area of an equilateral triangle.
+
+Console.WriteLine("Area of an equivalent triangle");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+double area = 1.73 / 4 * (a) * 2;
+
+Console.WriteLine(area);
